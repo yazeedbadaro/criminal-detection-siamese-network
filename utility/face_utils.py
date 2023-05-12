075ac9a66cbb97ba19f2e81d8d1e6fe06eaabd46
Yazeed Badaro <yazeed@Yazeeds-MacBook-Air.local>
2023-05-13 01:30:24 +0300

    added new yolo

--- utility/face_utils.py
+++ utility/face_utils.py
@@ -1,5 +1,4 @@
 import streamlit as st
-# from yoloface import face_analysis
 import cv2
 import os
 import subprocess
@@ -7,34 +6,9 @@ from streamlit_webrtc import webrtc_streamer
 import av
 from ultralytics import YOLO
 
-# @st.cache_resource
-# def start_yolo():
-#     return face_analysis()
-
-# face=start_yolo()
-
 face=YOLO("yolov8n-face.pt")
 
-
-# def image_face_detector(image):
-#     frame=image.copy()
-#     _, box,_ =face.face_detection(frame_arr=frame, frame_status=True, model='tiny')
-    
-#     i=1
-#     for (x, y, w, h) in box:
-#         cv2.rectangle(frame,(x,y),(x+w,y+h),(255, 0, 0),2)
-#         (w, h), _ = cv2.getTextSize("face", cv2.FONT_HERSHEY_DUPLEX, 0.6, 1)
-
-#         frame = cv2.rectangle(frame, (x, y - 20), (x + w, y), (255, 0, 0), -1)
-#         frame = cv2.putText(frame, "face", (x, y - 5),cv2.FONT_HERSHEY_DUPLEX, 0.6, (255,255,255), 1)
-
-#         try:
-#             cv2.imwrite(f"detected_faces/face{i}.jpg",frame[y:y+h, x:x+w])
-#             i=i+1
-#         except:
-#             print("empty frame error")
-#     return frame
-
+set
 
 def image_face_detector(image,flag=True):
     frame=image.copy()
