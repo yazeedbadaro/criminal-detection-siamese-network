4545b4e27abfb282033a60d2cdb9caa689bb7c96
yousef hamdan <hamdanyousef9@gmail.com>
2023-06-08 18:42:49 +0300

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -48,8 +48,7 @@ if authentication_status == None:
 
 if authentication_status:
     # ---- HEADER SECTION ----
-    #adding a button
-    
+    # Initializing Pinecone index
     pinecone.init(api_key= "5030e476-e093-4104-83d0-ec6f09ca7542", environment="northamerica-northeast1-gcp")
     index = pinecone.Index("grad-index")
 
@@ -62,7 +61,7 @@ if authentication_status:
 
     st.header(selected)
 
-    #How to use
+    # How to use section
     if selected=="How to use":
         
         st.subheader("Upload an Image or Video")
@@ -81,87 +80,92 @@ if authentication_status:
         st.subheader("Webcam")
         st.markdown("Click on the :red[RED START BUTTON] to start your camera then point it at the area where the detection will happen. Once you are done click on the 'detect faces' button and a list of detected faces will appear.")
 
-    #Image
+    # Upload Image section
     if selected=="Upload Image":
         empty_files()
-        uploaded_file = st.file_uploader("Choose an image",accept_multiple_files=False,type=["png","jpg","jpeg","pgm"])
+        uploaded_files = st.file_uploader("Choose an image",accept_multiple_files=True,type=["png","jpg","jpeg","pgm"])
 
-        if uploaded_file is not None:
-            st.image(uploaded_file)
-            st.image(image_face_detector(np.array(Image.open(uploaded_file).convert("RGB")),n=1,conf_thresh=0))
-            
-            #display results
-            for i,person in enumerate(glob("detected_faces/*.jpg")):
-                query_response = index.query(
-                                    top_k=3,
-                                    include_values=False,
-                                    include_metadata=True,
-                                    vector=get_image_embedding(person).tolist(),
-                                )
+        if uploaded_files is not None:
+            for uploaded_file in uploaded_files:
+                empty_files()
+                st.image(image_face_detector(np.array(Image.open(uploaded_file).convert("RGB")),n=1,conf_thresh=0))
                 
-                with st.expander(f"Person {i+1}"):
-                    col1,col2= st.columns(2)
-                    with col1:
-                        img=Image.open(person).resize((224,224))
-                        st.subheader("Detected person image")
-                        st.image(img) 
-            
-                    with col2:
-                        st.subheader("Criminal profile")
-                        cols= st.columns(3)
-                        
-                        for cn,col in enumerate(cols):
+                #display results
+                for i,person in enumerate(glob("detected_faces/*.jpg")):
+                    query_response = index.query(
+                                        top_k=3,
+                                        include_values=False,
+                                        include_metadata=True,
+                                        vector=get_image_embedding(person).tolist(),
+                                    )
+                    
+                    with st.expander(f"Person {i+1}"):
+                        col1,col2= st.columns(2)
+                        with col1:
+                            img=Image.open(person).resize((224,224))
+                            st.subheader("Detected person image")
+                            st.image(img) 
+                
+                        with col2:
+                            st.subheader("Criminal profile")
+                            cols= st.columns(3)
                             
-                            with col:
-                                st.image(Image.open(query_response['matches'][cn]["metadata"]["path"]).resize((224,224)))
-                                st.divider()
-                                st.markdown("Name: "+query_response['matches'][cn]["metadata"]["label"])
-                                st.markdown("Age: "+str(query_response['matches'][cn]["metadata"]["age"]))
-                                st.markdown("Gender: "+str(query_response['matches'][cn]["metadata"]["gender"]))
-                                st.markdown("Felony: "+str(query_response['matches'][cn]["metadata"]["felony"]))
+                            for cn,col in enumerate(cols):
+                                
+                                with col:
+                                    st.image(Image.open(query_response['matches'][cn]["metadata"]["path"]).resize((224,224)))
+                                    st.divider()
+                                    st.markdown("Name: "+query_response['matches'][cn]["metadata"]["label"])
+                                    st.markdown("Age: "+str(query_response['matches'][cn]["metadata"]["age"]))
+                                    st.markdown("Gender: "+str(query_response['matches'][cn]["metadata"]["gender"]))
+                                    st.markdown("Felony: "+str(query_response['matches'][cn]["metadata"]["felony"]))
 
 
-    #Video
-    if selected=="Upload Video":
+    # Upload Video section
+    if selected == "Upload Video":
         empty_files()
-        uploaded_file = st.file_uploader("Choose a video",accept_multiple_files=False,type=["mp4"])
+        uploaded_files = st.file_uploader("Choose videos", accept_multiple_files=True, type=["mp4", "avi", "mkv"])
 
-        if uploaded_file is not None:
-            video_face_detector(uploaded_file)
-            best_images("detected_faces",get_conf())
-            
-            #display results
-            for i,person in enumerate(glob("detected_faces/highest_score_images/*.jpg")):
-                query_response = index.query(
-                                    top_k=3,
-                                    include_values=False,
-                                    include_metadata=True,
-                                    vector=get_image_embedding(person).tolist(),
-                                )
-                
-                with st.expander(f"Person {i+1}"):
-                    col1,col2= st.columns(2)
-                    with col1:
-                        img=Image.open(person).resize((224,224))
-                        st.subheader("Detected person image")
-                        st.image(img) 
-            
-                    with col2:
-                        st.subheader("Criminal profile")
-                        cols= st.columns(3)
-                        
-                        for cn,col in enumerate(cols):
-                            
-                            with col:
-                                st.image(Image.open(query_response['matches'][cn]["metadata"]["path"]).resize((224,224)))
-                                st.divider()
-                                st.markdown("Name: "+query_response['matches'][cn]["metadata"]["label"])
-                                st.markdown("Age: "+str(query_response['matches'][cn]["metadata"]["age"]))
-                                st.markdown("Gender: "+str(query_response['matches'][cn]["metadata"]["gender"]))
-                                st.markdown("Felony: "+str(query_response['matches'][cn]["metadata"]["felony"]))
+        if uploaded_files is not None:
+            for uploaded_file in uploaded_files:
+                empty_files()
+                video_face_detector(uploaded_file)
+                best_images("detected_faces", get_conf())
+
+                # Display results for each uploaded file
+                for file_index, person in enumerate(glob("detected_faces/highest_score_images/*.jpg")):
+                    query_response = index.query(
+                        top_k=3,
+                        include_values=False,
+                        include_metadata=True,
+                        vector=get_image_embedding(person).tolist(),
+                    )
+
+                    with st.expander(f"Person {file_index + 1}"):
+                        col1, col2 = st.columns(2)
+                        with col1:
+                            img = Image.open(person).resize((224, 224))
+                            st.subheader("Detected person image")
+                            st.image(img)
+
+                        with col2:
+                            st.subheader("Criminal profile")
+                            cols = st.columns(3)
+
+                            for cn, col in enumerate(cols):
+                                with col:
+                                    st.image(
+                                        Image.open(query_response['matches'][cn]["metadata"]["path"]).resize((224, 224))
+                                    )
+                                    st.divider()
+                                    st.markdown("Name: " + query_response['matches'][cn]["metadata"]["label"])
+                                    st.markdown("Age: " + str(query_response['matches'][cn]["metadata"]["age"]))
+                                    st.markdown("Gender: " + str(query_response['matches'][cn]["metadata"]["gender"]))
+                                    st.markdown("Felony: " + str(query_response['matches'][cn]["metadata"]["felony"]))
+
 
         
-    #webcam
+    # Webcam section
     if selected=="Webcam":
 
         webrtc_streamer(key="example",video_frame_callback=video_frame_callback,media_stream_constraints={"video": True, "audio": False})
